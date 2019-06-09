540688909220e1a6f61c918fb58171f3a550c4bd
FAXES <31368070+FAXES@users.noreply.github.com>
2019-06-09 10:14:09 +1000

    Fixed old chat methods

--- server.lua
+++ server.lua
@@ -4,9 +4,6 @@ AddEventHandler("quitserver", function()
 	DropPlayer(playerId)
 end)
 
-AddEventHandler ('chatMessage', function(source, name, message)
-	if message == '/dc' then
-		DropPlayer(source, 'Thanks for stopping by. See you soon.')-- Change this message, It is displayed to the player on leave.
-		CancelEvent()
-	end
+RegisterCommand('dc', function()
+    DropPlayer(source, 'Thanks for stopping by. See you soon.')-- Change this message, It is displayed to the player on leave.
 end)
